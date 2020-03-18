3f492534e53077beacaa8e0ace0d9e0cb7d3c0c5
AlphaArtrem <yashawasthi01@gmail.com>
2020-03-18 21:26:38 +0530

    Updated README.md

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:worldclock/src/home.dart';
+import 'package:worldclock/src/loading.dart';
+import 'package:worldclock/src/location.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'World Clock',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => Location(),
+    },
+  ));
+}
