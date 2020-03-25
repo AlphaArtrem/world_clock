44905865e7660dfe7693eec5207a37de3b7e8fe7
AlphaArtrem <yashawasthi01@gmail.com>
2020-03-25 17:05:05 +0530

    Added functionaity to filter timzones using a search box

--- lib/src/location.dart
+++ lib/src/location.dart
@@ -10,9 +10,12 @@ class Location extends StatefulWidget {
 class _LocationState extends State<Location> {
 
   TimeZones tz = TimeZones();
+  List<String> locations, zones;
 
   void setup() async{
     tz.getTimeZones();
+    locations = tz.locations;
+    zones = tz.timezones;
   }
 
   void newTime(index, context) async {
@@ -40,24 +43,53 @@ class _LocationState extends State<Location> {
       ),
       body: Container(
         margin: EdgeInsets.fromLTRB(2, 5, 2, 2),
-        child: ListView.builder(
-          itemCount: tz.timezones.length,
-          itemBuilder: (context, index){
-            return Card(
-              color: Colors.deepPurpleAccent,
-              child: ListTile(
-                onTap: () {
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2),),
+                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple, width: 1),),
+                  hintText: "Search By Time Zone",
+                ),
+                onChanged: (val){
                   setState(() {
-                    newTime(index, context);
+                    locations = tz.locations.where((zone){
+                      zone = zone.toLowerCase();
+                      return zone.contains(val.toLowerCase());
+                    }).toList();
+
+                    zones = tz.timezones.where((zone){
+                      zone = zone.toLowerCase();
+                      return zone.contains(val.toLowerCase());
+                    }).toList();
                   });
                 },
-                title: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Text('${tz.locations[index]}', style: TextStyle(color: Colors.white, fontSize: 16, letterSpacing: 1.2, fontWeight: FontWeight.w300),),
-                ),
               ),
-            );
-          }
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: locations.length,
+                itemBuilder: (context, index){
+                  return Card(
+                    color: Colors.deepPurpleAccent,
+                    child: ListTile(
+                      onTap: () {
+                        setState(() {
+                          newTime(index, context);
+                        });
+                      },
+                      title: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text('${locations[index]}', style: TextStyle(color: Colors.white, fontSize: 16, letterSpacing: 1.2, fontWeight: FontWeight.w300),),
+                      ),
+                    ),
+                  );
+                }
+              ),
+            ),
+          ],
         ),
       ),
     );
